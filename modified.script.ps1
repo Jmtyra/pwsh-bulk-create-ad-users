de93011b4341cc49c927ead2cf870111eb098917
John Tyra <Jmtyra@users.noreply.github.com>
2021-11-30 23:00:04 -0600

    Fix for variables with email and domain
    Dummy domain used

--- modified.script.ps1
+++ modified.script.ps1
@@ -4,10 +4,10 @@ foreach ($User in $UserList)
     $Displayname = $User.Firstname + " " + $User.Lastname            
     $UserFirstname = $User.Firstname            
     $UserLastname = $User.Lastname            
-    $OU = "$User.OU"            
-    $SAM = $User.SAM            
-    $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain            
-    $Description = $User.Description            
-    $Password = $User.Password            
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server domain.loc            
+    # $OU = $User.OU
+    $SAM = $User.SAM
+    $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain
+    # $Description = $User.Description
+    $Password = $User.Password
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server customer.domain.com
 }
